029fb19d0fa593dfefb4a0978d3190b7a70d4864
William Selbie <wselbie6@gmail.com>
2018-12-31 23:29:53 -0800

    Added class fields. Added __str__ and basic methods.

--- minesweeper.py
+++ minesweeper.py
@@ -1,21 +1,41 @@
 import random
 
 class Board:
-    def __init__(self):
-        pass
-    
+    def __init__(self, width=10, height=10, mine_count=10, flag_count=10):
+        self.width = width
+        self.height = height
+        self.mine_count = mine_count
+        self.flag_count = flag_count
+        self.board = [[Tile() for x in range(self.width)] for y in range(self.height)]
+
     def __str__(self):
-        pass
+        return '\n'.join([''.join([str(tile) for tile in row]) for row in self.board])
 
 class Tile:
-    def __init__(self):
-        pass
+    def __init__(self, has_mine=False, has_flag=False, is_revealed=False, adjacent_tiles=[]):
+        self.has_mine = has_mine
+        self.has_flag = has_flag
+        self.is_revealed = is_revealed
+        self.adjacent_tiles = adjacent_tiles
+
+    def adjacent_mines(self):
+        return sum(1 if tile.has_mine else 0 for tile in self.adjacent_tiles)
+
+    def adjacent_revealed(self):
+        return sum(1 if tile.is_revealed else 0 for tile in self.adjacent_tiles) > 0
 
     def __str__(self):
-        pass
+        state = 'X'
+        if self.is_revealed:
+            state = 'O'
+        if self.has_flag:
+            state = 'F'
+        
+        return state  
 
 def main():
-    print("Hello World!")
+    board = Board()
+    print(board)
 
 if __name__ == "__main__":
     main()
