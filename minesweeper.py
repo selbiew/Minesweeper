39e33a317210bef2fc69a10c336b9737c283e213
William Selbie <wselbie6@gmail.com>
2018-12-31 22:27:06 -0800

    Added basic structure.

---
+++ minesweeper.py
@@ -0,0 +1,21 @@
+import random
+
+class Board:
+    def __init__(self):
+        pass
+    
+    def __str__(self):
+        pass
+
+class Tile:
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+def main():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
